60463d061b37e647a4f719937c430f865dbaf04f
ithzavg <ithgarvi@gmail.com>
2021-09-21 19:45:57 -0500

    Challenge completed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,50 @@
-import logo from './logo.svg';
+
 import './App.css';
+import {useState} from 'react';
 
 function App() {
+
+ 
+  const [disable, setDisable] = useState(false);
+
+  let obj = [{variant: "v1", data: ["cat-1", "cat-2", "cat-3"], isDefault: true},
+             {variant: "v2", data: ["cat-3", "cat-4", "cat-5"], isDefault: false}];
+
+
+
+  const Card = ({data}) => {
+    return (
+        <article  className="card">
+          <p>{data}</p>
+        </article>
+      )
+  }
+  
   return (
+
+  
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     {obj.map((value, idx) => {
+        return (
+          <div key = {idx} className="main-card">
+            <p className="main-card__title">{value.variant}</p>
+           
+            {
+              
+              value.data.map((data, key)=> {
+                return <Card key= {key} data={data}></Card>
+              })
+              
+            }
+            <button  className="main-card__btn" onClick={() => setDisable(!disable)}>
+              {value.isDefault === disable ? "Disable": "Enable"}
+            </button>
+            
+          </div>
+        )
+      
+     })}
+      
     </div>
   );
 }
